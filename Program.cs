0522c4f033e0f774b15218f26f9196e128abe512
Pavel Anokhin <anohinpa@gmail.com>
2022-10-18 20:13:53 +0300

    Task66

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task66
+int sumNaturalNumbers(int m, int n)
+{
+    if (m <= n) return m + sumNaturalNumbers(++m, n);
+    return 0;
+}
+Console.WriteLine(sumNaturalNumbers(1, 15));
